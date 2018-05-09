89ff131cba62b9303e763798509bd166c8998737
nguyening20 <36948494+nguyening20@users.noreply.github.com>
2018-05-08 17:26:49 -0700

    Blog type 1 structure

--- page-blog.php
+++ page-blog.php
@@ -67,21 +67,21 @@ get_header();
 		$myposts = get_posts('');
 		foreach($myposts as $post) :
 		setup_postdata($post);
+		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "large" );
+
 		?>
-		  <div class="post-item">
+		  <div class="post-item" style = "background-image: url(<?php echo $thumbnail[0] ?>);">
 		    <div class="post-info">
 		      <h2 class="post-title">
 		      <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
 		      <?php the_title(); ?>
 		      </a>
 		      </h2>
-		      <p class="post-meta">Posted by <?php the_author(); ?></p>
 		    </div>
 		    <div class="post-content">
 		    <?php the_content(); ?>
 		    </div>
 		  </div>
-		<?php comments_template(); ?>
 		<?php endforeach; wp_reset_postdata(); ?>
 	</div><!-- #primary -->
 
